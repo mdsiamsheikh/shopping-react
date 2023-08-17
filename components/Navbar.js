9dd18aa64daf19f5d16f70ee487ffe50bce28918
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2023-08-17 21:52:43 +0600

    first commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,33 +1,30 @@
-import Link from 'next/link'
-import React from 'react'
-import { FiShoppingCart } from "react-icons/fi"
-import { useRecoilState } from 'recoil'
-import { cartState } from '../atoms/cartState'
+import Link from "next/link";
+import React from "react";
+import { FiShoppingCart } from "react-icons/fi";
+// import { useRecoilState } from 'recoil'
+// import { cartState } from '../atoms/cartState'
 
 const Navbar = () => {
+  return (
+    <section className="shadow-xl bg-white sticky top-0">
+      <div className="container mx-auto py-2 text-[24px] flex justify-between items-center">
+        <div className="cursor-pointer">
+          <Link href="/">Logo</Link>
+        </div>
 
-    const [cartItem] = useRecoilState(cartState)
-
-    return (
-        <section className='shadow-xl bg-white sticky top-0'>
-            <div className='container mx-auto py-2 text-[24px] flex justify-between items-center'>
-                <div className='cursor-pointer'>
-                    <Link href="/">Logo</Link>
-                </div>
-
-                <div className='relative cursor-pointer'>
-                    <Link href="/cart">
-                        <div>
-                            <FiShoppingCart />
-                            <span className='absolute -top-2 -right-2 text-[13px] bg-red-600 h-[18px] w-[18px] rounded-full grid place-items-center text-white'>{cartItem.length}</span>
-                        </div>
-                    </Link>
-                </div>
-
+        <div className="relative cursor-pointer">
+          <Link href="/cart">
+            <div>
+              <FiShoppingCart />
+              <span className="absolute -top-2 -right-2 text-[13px] bg-red-600 h-[18px] w-[18px] rounded-full grid place-items-center text-white">
+                0
+              </span>
             </div>
-        </section>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-    )
-}
-
-export default Navbar
+export default Navbar;
