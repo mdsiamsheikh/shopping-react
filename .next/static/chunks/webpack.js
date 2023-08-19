89ee3a3a78b3e0a8c3e835e3f4c9478642cc0e7b
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2023-08-20 01:53:48 +0600

    remove the next js fille

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a9ad6eb3ab335bf6"; }
+/******/ 		__webpack_require__.h = function() { return "aef57158210794b7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
