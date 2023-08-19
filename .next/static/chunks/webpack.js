487ce50a4af09830ae31ad205ba19815d27f6b15
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2023-08-20 02:02:17 +0600

    chang the next.js fili

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "aef57158210794b7"; }
+/******/ 		__webpack_require__.h = function() { return "244d41c51d1a2635"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
