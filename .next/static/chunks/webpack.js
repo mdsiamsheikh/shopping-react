4308e3347be0db2927e3136f8cd503600f701567
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2023-12-12 17:45:26 +0600

    remove a comain

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2672f3229d64ee9c"; }
+/******/ 		__webpack_require__.h = function() { return "8f2f44b4a141972d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
